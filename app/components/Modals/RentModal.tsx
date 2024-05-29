86feb16a817cb6ded917c33809ee956371dd9c1a
Okellothomas <thomasolal33@gmail.com>
2024-05-29 12:45:17 +0300

    add changes

--- app/components/Modals/RentModal.tsx
+++ app/components/Modals/RentModal.tsx
@@ -15,6 +15,7 @@ import Input from "../Inputs/Input"
 import axios from "axios"
 import { useRouter } from "next/navigation"
 import toast from "react-hot-toast"
+import Lago from "../navbar/Lago"
  
 enum STEPS {
     CATEGORY = 0,
@@ -488,7 +489,7 @@ const RentModal = () => {
 
   return (
       <Modal
-          title="Devanca Hotels and House Listing!"
+          title={<Lago />}
           isOpen={rentModal.isOpen}
           onClose={rentModal.onClose}
           secondaryAction={step === STEPS.CATEGORY ? undefined: onBack}
