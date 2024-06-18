8ed7d706df32ccf3b8d8bfa3d03163cc54411538
Okellothomas <thomasolal33@gmail.com>
2024-06-18 08:38:02 +0300

    add changes

--- app/client/profile/components/SideBar.tsx
+++ app/client/profile/components/SideBar.tsx
@@ -4,23 +4,16 @@ import { FaSwatchbook } from "react-icons/fa";
 import { LiaSwatchbookSolid } from "react-icons/lia";
 import { IoBookmarks } from "react-icons/io5";
 import { FaRegAddressBook } from "react-icons/fa6";
-import { ImAddressBook } from "react-icons/im";
-import { PiAddressBookFill } from "react-icons/pi";
-import { LiaAddressBookSolid } from "react-icons/lia";
-import { PiBookmarksSimpleBold } from "react-icons/pi";
-import { PiBookOpenTextBold } from "react-icons/pi";
-import { PiBookOpenTextFill } from "react-icons/pi";
 import Link from "next/link";
 import "./sidebar.css";
-
-// Define the interface for the Home component props
+import { GiBed } from "react-icons/gi";
 
 
 // Home component is defined as an asynchronous function
 const ProfilePage = async () => {
   // Fetch listings and current user asynchronousl
   return (
-      <div className="border-[2px] sidebar-image-main rounded-xl px-5">
+      <div className="border-[2px] bg-white sidebar-image-main rounded-xl px-5">
           <div className="w-full flex justify-center text-center items-center py-5">
           <FaCircleUser className="text-neutral-500" size={ 50 } /> 
           </div>
@@ -35,8 +28,8 @@ const ProfilePage = async () => {
           <hr />
           </div>
            <div className="hover:font-semibold text-md flex cursor-pointer hover:bg-neutral-200 flex-row gap-3 py-4 items-center">
-          <Link href="/client/reservations"><LiaSwatchbookSolid className="text-neutral-500" size={26} /></Link>
-          <Link href="/client/reservations" className="sidebar-image">my reservations</Link>
+          <Link href="/client/reservations"><GiBed className="text-neutral-500" size={26} /></Link>
+          <Link href="/client/reservations" className="sidebar-image">My reservations</Link>
           </div>
           <div className="py-3">
           <hr />
